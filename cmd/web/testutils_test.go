34141a8b4a58a4793d2e39e4a79fb863a0456875
thaibh <thaibh@amcolab.vn>
2026-09-12 10:10:49 +0700

    page 535

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
+	"net/http/cookiejar"
 	"net/http/httptest"
 	"testing"
 )
@@ -26,6 +27,17 @@ type testServer struct {
 // of our custom testServer type.
 func newTestServer(t *testing.T, h http.Handler) *testServer {
 	ts := httptest.NewTLSServer(h)
+	jar, err := cookiejar.New(nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ts.Client().Jar = jar
+
+	ts.Client().CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
 	return &testServer{ts}
 }
 
